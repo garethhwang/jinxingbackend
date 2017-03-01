d7768b5b5b7bb35d8464edb9a1a0a612edfb94e0
hwang <hh698@126.com>
2017-03-01 18:15:16 +0800

    test

--- catalog/controller/wechat/physicalreceipt.php
+++ catalog/controller/wechat/physicalreceipt.php
@@ -7,7 +7,7 @@ class ControllerWechatPhysicalReceipt extends Controller
 
     public function submit(){
 
-        $log = new Log("wechat.log");
+        //$log = new Log("wechat.log");
         $this->session->data['openid']='oKe2EwWLwAU7EQu7rNof5dfG1U8g';
         if (isset($this->session->data['openid'])) {
             //$log->write("PersonalCenter openid:" . $this->session->data['openid']);
