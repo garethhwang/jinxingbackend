ea9a025b0a05681ace7d6bd5886f2e90c5c8f688
hwang <hh698@126.com>
2017-03-01 21:51:06 +0800

    test

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -175,10 +175,6 @@ class ControllerProductProduct extends Controller
 
         $product_info['images'] = array();
         $product_info['images'] = $this->model_catalog_product->getProductImages($product_id);
-        foreach ($product_info['images']['image'] as $image) {
-            $image = "image/" . $image;
-        }
-        $product_info['images']['image'][] = $image;
         $product_info['description'] = html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8');
         $product_info['service_notes'] = html_entity_decode($product_info['service_notes'], ENT_QUOTES, 'UTF-8');
         $product_info['applicable_user'] = html_entity_decode($product_info['applicable_user'], ENT_QUOTES, 'UTF-8');
