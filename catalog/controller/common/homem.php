dc76cb2426de68bd2f02c5188781636fd7949e67
hwang <hh698@126.com>
2017-03-04 01:43:45 +0800

    order

--- catalog/controller/common/homem.php
+++ catalog/controller/common/homem.php
@@ -15,7 +15,7 @@ class ControllerCommonHomem extends Controller
         $log = new Log("wechat.log");
         $data["error_warning"] = "";
         $get_return = array();
-        //$this->session->data['openid']='oKe2EwVNWJZA_KzUHULhS1gX6tZQ';
+        $this->session->data['openid']='oKe2EwVNWJZA_KzUHULhS1gX6tZQ';
         $code = $this->request->get["code"];
 
         if (isset($code)) {
