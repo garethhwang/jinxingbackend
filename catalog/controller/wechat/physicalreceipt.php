9fdbe22a8c14a4d113a212133dc1409b0557ce6a
hwang <hh698@126.com>
2017-03-05 00:21:27 +0800

    order

--- catalog/controller/wechat/physicalreceipt.php
+++ catalog/controller/wechat/physicalreceipt.php
@@ -239,7 +239,7 @@ class ControllerWechatPhysicalReceipt extends Controller
             );
 
             $this->load->model('wechat/physicalreceipt');
-            //$this->model_wechat_physicalreceipt->addReceiptHistory($result);
+            $this->model_wechat_physicalreceipt->addReceiptHistory($result);
             $record = $this->model_wechat_physicalreceipt->getRecord($data['customer_id']);
             $this->load->model('account/customer');
             if ($record == '1') {
