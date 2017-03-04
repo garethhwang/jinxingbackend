f77850033b8ddd428c03748b9eeac954d8ebeba6
hwang <hh698@126.com>
2017-03-05 00:11:19 +0800

    order

--- catalog/controller/wechat/wechatbinding.php
+++ catalog/controller/wechat/wechatbinding.php
@@ -57,7 +57,7 @@ class ControllerWechatWechatbinding extends Controller
         $log = new Log("wechat.log");
 
         $data["openid"] = "";
-	    $code = $this->request->json('code',0);
+	    $code = $this->request->json('code');
         //wechat
         if (isset($code)) {
             try {
