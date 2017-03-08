00742daa0dd539f8a9cea99ef5898da96c7ce84d
hwang <hh698@126.com>
2017-03-08 23:47:24 +0800

    delete

--- catalog/controller/wechat/orderDetail.php
+++ catalog/controller/wechat/orderDetail.php
@@ -122,15 +122,9 @@ class ControllerWechatOrderDetail extends Controller
                 if($coupon_info['type'] == 'P'){
                     $data["coupontype"] = "P";
                 }
-                $coupon = $this->model_extension_total_coupon-> getTotal($product_info['products'][0], $coupon_info['code'],$product_info['products'][0]['product_id'],$order_info['customer_id']);
+                //$coupon = $this->model_extension_total_coupon-> getTotal($product_info['products'][0], $coupon_info['code'],$product_info['products'][0]['product_id'],$order_info['customer_id']);
                 $data['discount'] =  floatval($coupon_info['discount']);
-
-                $log->write("total111=".$product_info['products'][0]['total']);
-                $log->write("tota2222=".$coupon_info['code']);
-                $log->write("tota3333=".$product_info['products'][0]['product_id']);
-                $log->write("tota444=".$order_info['customer_id']);
-                $log->write("total123=".$coupon['total']);
-                $data['lastprice'] = floatval($coupon['total']);
+                $data['lastprice'] = floatval($order_info['total']);
                 //$log->write("lastprice".$data['lastprice']);
                 //unset($this->session->data['coupon']);
             }else{
