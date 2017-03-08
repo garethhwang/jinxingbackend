519853706ec37edcfbf47f10bcea79e6216f130f
hwang <hh698@126.com>
2017-03-08 23:21:00 +0800

    delete

--- catalog/model/extension/total/coupon.php
+++ catalog/model/extension/total/coupon.php
@@ -132,8 +132,6 @@ class ModelExtensionTotalCoupon extends Model {
 
                 $discount_total = 0;
 
-                $log->write("12312312");
-
                 /*if (!$coupon_info['product']) {
                     $sub_total = $this->cart->getSubTotal();
                 } else {
@@ -216,7 +214,6 @@ class ModelExtensionTotalCoupon extends Model {
                     $discount_total = $total;
                 }
 
-                $log->write("discount_total=".$discount_total);
 
                 if ($discount_total > 0) {
 
@@ -227,10 +224,11 @@ class ModelExtensionTotalCoupon extends Model {
                         'sort_order' => $this->config->get('coupon_sort_order')
                     );
 
-                    $log->write("_total=".$total['total']);
+
 
                     $total['total'] -= $discount_total;
 
+                    $log->write("_total=".$total['total']);
 
 
                 }
