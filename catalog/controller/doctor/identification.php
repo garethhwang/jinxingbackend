3541c347f9462faf6a8883a50566669362bce34f
hwang <hh698@126.com>
2017-03-13 18:56:01 +0800

    upload

--- catalog/controller/doctor/identification.php
+++ catalog/controller/doctor/identification.php
@@ -129,25 +129,29 @@ class ControllerDoctorIdentification extends Controller
             }
             else
             {
-                //$fileurl = $this->createIdentificationUrl($customer_id);
+                $fileurl = $this->createIdentificationUrl($customer_id);
+                $date = date("Y-m-d");
+                $filename = $doctor_id.$customer_id.$date.$_FILES["file"]["name"];
+                $filename = md5($filename);
                 // 判断当期目录下的 upload 目录是否存在该文件
                 // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-                if (file_exists("image/" . $_FILES["file"]["name"]))
-                {
+                //if (file_exists("image/" . $_FILES["file"]["name"]))
+                //{
                     //echo $_FILES["file"]["name"] . " 文件已经存在。 ";
-                }
-                else
-                {
+                //}
+                //else
+                //{
                     // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-                    move_uploaded_file($_FILES["file"]["tmp_name"], "image/" . $_FILES["file"]["name"]);
+                    move_uploaded_file($_FILES["file"]["tmp_name"], $fileurl.$filename);
                     //echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
-                }
+                //}
 
                 $result = array(
-                    'filename' => $_FILES["file"]["name"],
+                    'fileoriginname' => $_FILES["file"]["name"],
+                    'filename' => $filename,
                     'filetype' => $_FILES["file"]["type"],
                     'filesize' => ($_FILES["file"]["size"] / 1024),
-                    'fileurl' => "image/".$_FILES["file"]["name"]
+                    'fileurl' => $fileurl.$filename
                 );
 
 
@@ -201,7 +205,7 @@ class ControllerDoctorIdentification extends Controller
             chmod("image/identification".$date."/".$data['realname'], 0777);
         }
 
-        $url = "image/identification".$date."/".$data['realname'] ;
+        $url = "image/identification/".$date."/".$data['realname']."/" ;
 
         return  $url ;
 
--- catalog/controller/doctor/register.php
+++ catalog/controller/doctor/register.php
@@ -118,7 +118,7 @@ class ControllerDoctorRegister extends Controller
         } else {
             $data['isnotright'] = '0';
 
-            $customer_id = $this->model_doctor_doctor->addDoctor($postdata);
+            $data["doctor_id"] = $this->model_doctor_doctor->addDoctor($postdata);
             //$this->customer->wechatlogin($data["openid"]);
             //unset($this->session->data['guest']);
             //$this->response->redirect($this->url->link('wechat/registersuccess', '', true));
