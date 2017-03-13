a09b81e1f87c54c4b785454e645659268020638a
hwang <hh698@126.com>
2017-03-13 19:16:16 +0800

    upload

--- catalog/controller/doctor/register.php
+++ catalog/controller/doctor/register.php
@@ -118,7 +118,7 @@ class ControllerDoctorRegister extends Controller
 
             $this->load->model('doctor/doctor');
 
-            //$data["doctor_id"] = $this->model_doctor_doctor->addDoctor($postdata);
+            $data["doctor_id"] = $this->model_doctor_doctor->addDoctor($postdata);
             //$this->customer->wechatlogin($data["openid"]);
             //unset($this->session->data['guest']);
             //$this->response->redirect($this->url->link('wechat/registersuccess', '', true));
