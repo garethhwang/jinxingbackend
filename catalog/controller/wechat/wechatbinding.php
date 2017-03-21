3037f9216f73f7e0dac59e88397c3bfac47ba4ba
hwang <hh698@126.com>
2017-03-21 16:00:05 +0800

    login

--- catalog/controller/wechat/wechatbinding.php
+++ catalog/controller/wechat/wechatbinding.php
@@ -143,7 +143,7 @@ class ControllerWechatWechatbinding extends Controller
             $this->load->model('wechat/userinfo');
             $temp = $this->model_wechat_userinfo->getUserInfo($data["openid"]);
 
-            $log->write("openid=".$data["openid"]."temp=".$temp["wechat_id"]);
+            $log->write("openid=".$data["openid"]."temp=".$temp[0]["wechat_id"]);
 
             if(!$temp){
                 $response = array(
