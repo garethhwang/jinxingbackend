d4c3b4e69c6f4372b09d3e60778349bd645e3a20
hwang <hh698@126.com>
2017-03-21 15:54:28 +0800

    login

--- catalog/controller/wechat/puerpera.php
+++ catalog/controller/wechat/puerpera.php
@@ -149,6 +149,8 @@ class ControllerWechatPuerpera extends Controller
         $this->load->model('wechat/userinfo');
         $temp = $this->model_wechat_userinfo->getUserInfo($data["openid"]);
 
+        $log->write("openid=".$data["openid"]."temp=".$temp);
+
         if(!$temp){
             $response = array(
                 'code'  => 1031,
