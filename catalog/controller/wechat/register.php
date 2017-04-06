5f1c12551ebbf1cd39c6e1c3837b823b4f3bb045
hwang <hh698@126.com>
2017-04-07 07:07:47 +0800

    test

--- catalog/controller/wechat/register.php
+++ catalog/controller/wechat/register.php
@@ -56,7 +56,7 @@ class ControllerWechatRegister extends Controller
         $log = new Log("wechat.log");
 
         $data["jxsession"] = $this->load->controller('account/authentication');
-        if(empty($data["jxsession"])) {
+        /*if(empty($data["jxsession"])) {
             $response = array(
                 'code'  => 1002,
                 'message'  => "欢迎来到金杏健康，请您先登录",
@@ -66,7 +66,7 @@ class ControllerWechatRegister extends Controller
             $this->response->addHeader('Content-Type: application/json');
             $this->response->setOutput(json_encode($response));
             return ;
-        }
+        }*/
         $customer_info = json_decode($this->cache->get($data["jxsession"]),true);
 
         /*if(isset($this->session->data['openid'])){
