d69bf43298d0a953f569bb02caafc621a36b1563
hwang <hh698@126.com>
2017-04-07 07:38:09 +0800

    test

--- catalog/controller/account/authentication.php
+++ catalog/controller/account/authentication.php
@@ -6,6 +6,7 @@ class ControllerAccountAuthentication extends Controller {
 
         $log = new Log('wechat.log');
         $jxsession = $this->request->json("jxsession",0);
+        $code = $this->request->json("code",0);
 
         $log->write("session=".$jxsession."   code=".$code);
 
