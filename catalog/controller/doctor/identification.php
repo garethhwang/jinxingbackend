fff126ad5700c94f24ca27dfbc996bcefeffce3b
Geo <zzc_workspace@126.com>
2017-04-26 17:35:41 +0800

    test

--- catalog/controller/doctor/identification.php
+++ catalog/controller/doctor/identification.php
@@ -259,13 +259,14 @@ class ControllerDoctorIdentification extends Controller
 
         //$customer_id = $this->request->json("customer_id");
 
+        $log->write("file==  ".$_FILES["file"]["name"]);
+        return ;
 
         $allowedExts = array("gif", "jpeg", "jpg", "png");
         $temp = explode(".", $_FILES["file"]["name"]);
         $extension = end($temp);// 获取文件后缀名
 
-        $log->write("file==  ".$_FILES["file"]["name"]);
-        return ;
+
 
         if ((($_FILES["file"]["type"] == "image/gif")
                 || ($_FILES["file"]["type"] == "image/jpeg")
